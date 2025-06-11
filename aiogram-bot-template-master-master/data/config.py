176a10a327892cc643cac4962b0a024dca4e83fa
AdilxanovAbbaz <adilxanovabbaz1@gmail.com>
2025-06-11 10:56:33 +0500

    tg-bot

--- aiogram-bot-template-master-master/data/config.py
+++ aiogram-bot-template-master-master/data/config.py
@@ -5,6 +5,6 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+BOT_TOKEN = env.str("8052593935:AAGDugllAbO5347v3ihUZVp3nHGfRGdC9yw")  # Bot toekn
+ADMINS = env.list("7625864389")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
